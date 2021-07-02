90ce6e28a8dfa9c47739157605c03525d1abfcd7
unknown <gshgsh0831@fasoo.com>
2021-07-02 13:19:54 +0900

    for rollback

--- src/main/java/me/soungho/BlogAPIServer/CustomException/ExceptionAdvice.java
+++ src/main/java/me/soungho/BlogAPIServer/CustomException/ExceptionAdvice.java
@@ -1,7 +1,5 @@
 package me.soungho.BlogAPIServer.CustomException;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
